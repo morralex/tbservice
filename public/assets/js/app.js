a89b1388c95479a59ade7faca4c7ca4bbe0340f5
Alex Morales <morr.alex@outlook.com>
2021-10-31 13:13:57 -0700

    initialPush

---
+++ public/assets/js/app.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instance = M.Carousel.init({
+        fullWidth: true,
+        indicators: true
+      });
+
+
+  });
+
+
+
+
